9a14ce20c6460c54d27790f47198b7354a981b05
Husain007 <husainbhattiwala5253@gmail.com>
2023-10-14 13:16:43 +0530

    Fixed Hero section links

--- src/components/sections/hero/Stats.jsx
+++ src/components/sections/hero/Stats.jsx
@@ -4,25 +4,25 @@ import { Eye, ThumbsUp, User, Video } from "lucide-react";
 const Stats = ({}) => {
   return (
     <div className='hidden md:flex flex-col w-max px-4 gap-5 text-info'>
-      <List link='/instagram' sizes='xs' className='gap-4'>
+      <List link='/' sizes='xs' className='gap-4'>
         <User size={16} />
         <span>
           <b className='text-secondary'>106k</b> Followers
         </span>
       </List>
-      <List link='/instagram' sizes='xs' className='gap-4'>
+      <List link='/' sizes='xs' className='gap-4'>
         <Video size={16} />
         <span>
           <b className='text-secondary'>102</b> Videos
         </span>
       </List>
-      <List link='/instagram' sizes='xs' className='gap-4'>
+      <List link='/' sizes='xs' className='gap-4'>
         <Eye size={16} />
         <span>
           <b className='text-secondary'>100M</b> Views
         </span>
       </List>
-      <List link='/instagram' sizes='xs' className='gap-4'>
+      <List link='/' sizes='xs' className='gap-4'>
         <ThumbsUp size={16} />
         <span>
           <b className='text-secondary'>28.3M</b> Likes
